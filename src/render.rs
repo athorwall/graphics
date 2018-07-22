fde0a8c7ea5cdef1c45b27a27419dd847f9f07f0
Andrew Thorwall <anthor@umich.edu>
2018-07-21 22:37:08 -0700

    begin refactor

--- src/render.rs
+++ src/render.rs
@@ -20,15 +20,6 @@ pub fn render_mesh(
     ambient: &FloatColor,
     texture: Option<&Texture>,
     material: &Material,
-    fragment_processor: &Fn(
-        &Vector4<f32>,
-        &Vector4<f32>,
-        &Vector2<f32>,
-        &Vec<Light>,
-        &FloatColor,
-        &Option<&Texture>,
-        &Material,
-    ) -> FloatColor,
 ) {
     let world_to_clip_space = *camera_to_clip_space * *world_to_camera_space;
     for (w0, w1, w2) in &mesh.vertices {
@@ -51,7 +42,6 @@ pub fn render_mesh(
                 ambient,
                 texture,
                 material,
-                fragment_processor,
             );
         }
     }
@@ -67,15 +57,6 @@ fn render_triangle(
     ambient: &FloatColor,
     texture: Option<&Texture>,
     material: &Material,
-    fragment_processor: &Fn(
-        &Vector4<f32>,
-        &Vector4<f32>,
-        &Vector2<f32>,
-        &Vec<Light>,
-        &FloatColor,
-        &Option<&Texture>,
-        &Material,
-    ) -> FloatColor,
 ) {
     let clip_to_camera_space = camera_to_clip_space.invert().unwrap();
     let camera_to_world_space = world_to_camera_space.invert().unwrap();
@@ -93,7 +74,61 @@ fn render_triangle(
         (camera0, camera1, camera2),
         (perspective_adjusted0, perspective_adjusted1, perspective_adjusted2),
         &|coords, normals, uvs| {
-            fragment_processor(coords, normals, uvs, lights, ambient, &texture, material)
+            process_fragment(coords, normals, uvs, lights, ambient, &texture, material)
         },
     );
+}
+
+fn process_fragment(
+    world_coordinates: &Vector4<f32>,
+    world_normals: &Vector4<f32>,
+    uvs: &Vector2<f32>,
+    lights: &Vec<Light>,
+    ambient: &FloatColor,
+    texture: &Option<&Texture>,
+    material: &Material,
+) -> FloatColor {
+    let texture_color = match texture {
+        Some(ref t) => {
+            FloatColor::from_sdl_color(&t.sample(uvs.x, uvs.y, TextureFilterMode::NearestNeighbor))
+        },
+        None => FloatColor::from_rgb(1.0, 1.0, 1.0),
+    };
+    let color_from_lights = lights.iter()
+        .map(|light| {
+            process_fragment_light(
+                &world_coordinates,
+                &world_normals,
+                light,
+                material,
+            )
+        })
+        .sum();
+    let ambient_color = FloatColor::multiply_colors(ambient, &material.ambient);
+    let total_light_color = ambient_color + color_from_lights;
+
+    let net_color = FloatColor::multiply_colors(&texture_color, &total_light_color);
+    net_color.clamped()
+}
+
+fn process_fragment_light(
+    world_coordinates: &Vector4<f32>,
+    world_normals: &Vector4<f32>,
+    light: &Light,
+    material: &Material,
+) -> FloatColor {
+    let intensity = match light.light_type {
+        LightType::Directional(ref directional_light) => {
+            let intensity = directional_light.direction.dot(world_normals.truncate());
+            if intensity < 0.0 { 0.0 } else { intensity }
+        }
+        LightType::Point(ref point_light) => {
+            let ray = point_light.position - world_coordinates.truncate();
+            let distance = ray.magnitude();
+            let normalized_ray = ray / distance;
+            let intensity = normalized_ray.dot(world_normals.truncate()) / (distance * distance);
+            if intensity < 0.0 { 0.0 } else { intensity }
+        }
+    };
+    FloatColor::multiply_colors(&material.diffuse, &light.color) * intensity
 }
